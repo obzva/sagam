be28e9246ad8aef31944b8f2df8846679785e5cf
obzva <dngyng1000@gmail.com>
2025-03-27 11:21:54 -0700

    fix main.go

--- cmd/sagam/main.go
+++ cmd/sagam/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 	"time"
@@ -9,17 +10,14 @@ import (
 )
 
 func main() {
-	// sFlag := flag.Int("s", 25, "sprint duration in minutes, default is 25")
-	// rFlag := flag.Int("r", 5, "rest duration in minutes, default is 5")
-	// flag.Parse()
+	sFlag := flag.Int("s", 25, "sprint duration in minutes, default is 25")
+	rFlag := flag.Int("r", 5, "rest duration in minutes, default is 5")
+	flag.Parse()
 
-	// sprintDuration := time.Duration(*sFlag) * time.Minute
-	// restDuration := time.Duration(*rFlag) * time.Minute
+	sprintDuration := time.Duration(*sFlag) * time.Minute
+	restDuration := time.Duration(*rFlag) * time.Minute
 
-	sprintDuration := 10 * time.Second
-	restDuration := 5 * time.Second
 	if err := sagam.Run(os.Stdin, os.Stdout, sprintDuration, restDuration); err != nil {
 		log.Fatal(err)
 	}
-
 }
